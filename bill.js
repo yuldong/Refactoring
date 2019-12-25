c6bfd8d69b7111b642560d450be450d24eb87a3b
yuldong <yuldong@126.com>
2019-12-25 17:13:03 +0800

    version 1.1 提取函数，命名规范

--- bill.js
+++ bill.js
@@ -1,5 +1,4 @@
 function statement(invoice, plays) {
-    console.log(invoice);
     let totalAmount = 0;
     let volumentCredit = 0;
     let result = 'Statement for ${invoice.customer}\n';
@@ -9,28 +8,7 @@ function statement(invoice, plays) {
 
     for (let perf in invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "trageday":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 100 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 +  500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-        
-            default:
-                throw new Error('unknow type: ${play.type}');
-                break;
-        }
-
+        let thisAmount = amountFor(perf, play);
         volumentCredit += Math.max(perf.audience - 30, 0);
         if ("comedy" == play.type) {
             volumentCredit += Math.floor(perf.audience / 5);
@@ -41,6 +19,29 @@ function statement(invoice, plays) {
     }
     result += 'Amount owed is ${format(totalAmount / 100)}\n';
     result += 'You earned ${volumentCredit} credits';
-    console.log("shfshdfsf");
     return result;
+}
+
+function amountFor(perf, play) {
+    let result = 0;
+
+    switch (play.type) {
+        case "trageday":
+            result = 40000;
+            if (perf.audience > 30) {
+                result += 100 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (perf.audience > 20) {
+                result += 10000 +  500 * (perf.audience - 20);
+            }
+            result += 300 * perf.audience;
+            break;
+    
+        default:
+            throw new Error('unknow type: ${play.type}');
+    }
+    return result
 }
